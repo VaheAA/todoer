b631dba6fcf0a33516014ea798d2b5e685cfc213
VaheAA <vahe.abov@gmail.com>
2022-04-01 16:30:03 +0400

    create todo form

---
+++ client/src/components/AddList.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="form" @submit.prevent="handleClick">
+    <h1 class="form__title">{{ title }}</h1>
+    <div class="form__group">
+      <label class="form__label"> List name</label>
+      <input
+        :value="listName"
+        @input="$emit('update:listName', $event.target.value)"
+        class="form__input"
+        type="email"
+        required
+      />
+    </div>
+    <button class="btn submit__btn">{{ btnText }}</button>
+  </form>
+</template>
+
+<script setup>
+const propst = defineProps({
+  listName: String,
+  title: String,
+  btnText: String
+});
+
+const emit = defineEmits(['update:listName', 'handleSubmit']);
+
+const handleClick = () => {
+  emit('handleSubmit');
+};
+</script>
+
+<style lang="scss"></style>
--- client/src/views/CreateList.vue
+++ client/src/views/CreateList.vue
@@ -1,7 +1,16 @@
 <template>
-  <div></div>
+  <main class="main">
+    <div class="container">
+      <AddList title="Add new List" btnText="Create list" v-model="listInput" />
+    </div>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import AddList from '../components/AddList.vue';
+import { ref } from 'vue';
 
-<style lang="scss" scoped></style>
+const listInput = ref('');
+</script>
+
+<style lang="scss"></style>
